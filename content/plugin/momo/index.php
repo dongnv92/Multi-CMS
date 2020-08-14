37168845fc8878175e114b1a9317bdef5a3ace14
Dong Nguyen <nguyenvandong242@gmail.com>
2020-08-14 17:27:29 +0700

    ...

--- content/plugin/momo/index.php
+++ content/plugin/momo/index.php
@@ -4,7 +4,7 @@ switch ($path[2]){
         $header['title'] = 'Lịch sử giao dịch';
         require_once ABSPATH . PATH_ADMIN . "/admin-header.php";
         echo admin_breadcrumbs('MOMO', 'Lịch sử giao dịch','Lịch sử', [URL_ADMIN . "/{$path[1]}/" => 'MOMO']);
-        
+
         require_once ABSPATH . PATH_ADMIN . "/admin-footer.php";
         break;
 }
--- test.php
+++ test.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Upload your files</title>
-</head>
-<body>
-<form enctype="multipart/form-data" action="" method="POST">
-    <p>Upload your file</p>
-    <input type="file" name="uploaded_file"></input><br />
-    <input type="submit" value="Upload"></input>
-</form>
-</body>
-</html>
-<?PHP
-if(!empty($_FILES['uploaded_file']))
-{
-    print_r($_FILES['uploaded_file']);
+<?php
+function encryptDecrypt($data, $key, $mode = 'ENCRYPT'){
+    if (strlen($key) < 32) {
+        $key = str_pad($key, 32, 'x');
+    }
+    $key = substr($key, 0, 32);
+    $iv = pack('C*', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    if ($mode === 'ENCRYPT') {
+        return base64_encode(openssl_encrypt($data, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv));
+    }
+    else {
+        return openssl_decrypt(base64_decode($data), 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+    }
 }
-?>
+
+
