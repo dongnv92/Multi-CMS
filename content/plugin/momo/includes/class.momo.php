c3738141cba234b311d86f775c32f79bb64afd99
Dong Nguyen <nguyenvandong242@gmail.com>
2020-08-20 17:00:04 +0700

    ...

--- content/plugin/momo/includes/class.momo.php
+++ content/plugin/momo/includes/class.momo.php
@@ -4,10 +4,9 @@
  * $otp                 : Lấy khi đăng nhập mới từ đầu.
  * $rkey                : Lấy trong publiv/login
  * $setupKeyEncrypted   : Lấy trong public
- * */
-class Momo{
-    private $config;
-    private $phone              = '0966624292';
+ *
+ *
+ *  private $phone              = '0966624292';
     private $password           = '241992';
     private $otp                = '761912'; // (*) mã OTP gửi về điện thoại khi login
     private $rkey               = 'hM7iCzZHMpB1JkRdZXHM'; // (*) 20 ký tự, xem trong public/login
@@ -16,17 +15,20 @@ class Momo{
     private $token              = 'dfdjCewLGlE:APA91bEyR_lpb8CN6eghznFGMuPzSPpr9qb7Z8SlBJa3zeReBopzKQvsdf7QAkVBEnWKK3dX-uyFsPPy0Yrsbxq3Gh6KzqdFDnXGjNrzK5FeXwtPfhO8cfYgvjVWCxBZpIaXzBhVf7Lc';
     private $onesignalToken     = 'a12e8af7-4f94-4fbd-9983-64aa2e938ac5';
 
-    public function __construct(){
-        $ohash = hash('sha256', $this->phone . $this->rkey . $this->otp);
+ * */
+class Momo{
+    private $config;
+    public function __construct($phone, $password, $otp, $rkey, $setupKeyEncrypted, $imei, $token, $onesignalToken){
+        $ohash = hash('sha256', $phone . $rkey . $otp);
         $this->config = [
-            'phone'                 => $this->phone,
-            'otp'                   => $this->otp,
-            'password'              => $this->password,
-            'rkey'                  => $this->rkey,
-            'setupKeyEncrypted'     => $this->setupKeyEncrypted,
-            'imei'                  => $this->imei,
-            'token'                 => $this->token,
-            'onesignalToken'        => $this->onesignalToken,
+            'phone'                 => $phone,
+            'otp'                   => $otp,
+            'password'              => $password,
+            'rkey'                  => $rkey,
+            'setupKeyEncrypted'     => $setupKeyEncrypted,
+            'imei'                  => $imei,
+            'token'                 => $token,
+            'onesignalToken'        => $onesignalToken,
             'aaid'                  => '',
             'idfa'                  => '',
             'csp'                   => 'Viettel',
@@ -45,7 +47,7 @@ class Momo{
             'appVer'                => '21481',
             'appCode'               => "2.1.48",
             'deviceOS'              => "IOS",
-            'setupKeyDecrypted'     => $this->encryptDecrypt($this->setupKeyEncrypted, $ohash, 'DECRYPT')
+            'setupKeyDecrypted'     => $this->encryptDecrypt($setupKeyEncrypted, $ohash, 'DECRYPT')
         ];
     }
 
@@ -164,6 +166,138 @@ class Momo{
         $ch = curl_init();
         curl_setopt_array($ch, array(
             CURLOPT_URL             => "https://owa.momo.vn/api/sync/$type",
+            CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_ENCODING        => "",
+            CURLOPT_MAXREDIRS       => 10,
+            CURLOPT_TIMEOUT         => 30,
+            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST   => "POST",
+            CURLOPT_POSTFIELDS      => json_encode($data_post),
+            CURLOPT_HTTPHEADER      => array(
+                'User-Agent'    => "MoMoApp-Release/%s CFNetwork/978.0.7 Darwin/18.6.0",
+                'Msgtype'       => $type,
+                'Userhash'      => md5($config['phone']),
+                'Accept'        => 'application/json',
+                'Content-Type'  => 'application/json',
+                'Authorization: Bearer ' . trim($this->get_auth()),
+            )
+        ));
+        $result = curl_exec($ch);
+        if(!$result){
+            return false;
+        }
+        return $result;
+    }
+
+    public function oder_cash($phone, $name, $comment, $money) {
+        $config = $this->config;
+        $type   = 'M2MU_INIT';
+        $data_post = [
+            'user'      => $config['phone'],
+            'msgType'   => $type,
+            'cmdId'     => $this->get_microtime() . '000000',
+            'lang'      => $config['lang'],
+            'channel'   => $config['channel'],
+            'time'      => $this->get_microtime(),
+            'appVer'    => $config['appVer'],
+            'appCode'   => $config['appCode'],
+            'deviceOS'  => $config['deviceOS'],
+            'result'    => true,
+            'errorCode' => 0,
+            'errorDesc' => '',
+            'extra' => [
+                'checkSum' => $this->get_checksum($type)
+            ],
+            'momoMsg' => [
+                '_class' => 'mservice.backend.entity.msg.M2MUInitMsg',
+                'ref' => '',
+                'tranList' => [
+                    0 => [
+                        '_class'            => 'mservice.backend.entity.msg.TranHisMsg',
+                        'tranType'          => 2018,
+                        'partnerId'         => $phone,
+                        'originalAmount'    => $money,
+                        'comment'           => $comment,
+                        'moneySource'       => 1,
+                        'partnerCode'       => 'momo',
+                        'partnerName'       => $name,
+                        'rowCardId'         => NULL,
+                        'serviceMode'       => 'transfer_p2p',
+                        'serviceId'         => 'transfer_p2p',
+                        'extras'            => '{"vpc_CardType":"SML","vpc_TicketNo":"115.79.139.158","receiverMembers":[{"receiverNumber":"'.$phone.'","receiverName":"'.$name.'","originalAmount":'.$money.'}],"loanId":0,"contact":{}}',
+                    ],
+                ],
+            ],
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL             => "https://owa.momo.vn/api/$type",
+            CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_ENCODING        => "",
+            CURLOPT_MAXREDIRS       => 10,
+            CURLOPT_TIMEOUT         => 30,
+            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST   => "POST",
+            CURLOPT_POSTFIELDS      => json_encode($data_post),
+            CURLOPT_HTTPHEADER      => array(
+                'User-Agent'    => "MoMoApp-Release/%s CFNetwork/978.0.7 Darwin/18.6.0",
+                'Msgtype'       => $type,
+                'Userhash'      => md5($config['phone']),
+                'Accept'        => 'application/json',
+                'Content-Type'  => 'application/json',
+                'Authorization: Bearer ' . trim($this->get_auth()),
+            )
+        ));
+        $result = curl_exec($ch);
+        if(!$result){
+            return false;
+        }
+        $id = json_decode($result, true);
+        $id = $id['momoMsg']['replyMsgs'][0]['ID'];
+        $send   = $this->comfirm_oderr($id);
+        if(!$send){
+            return false;
+        }
+        return $send;
+    }
+
+    private function comfirm_oderr($id) {
+        $config = $this->config;
+        $type   = 'M2MU_CONFIRM';
+
+        $data_post = [
+            'user'      => $config['phone'],
+            'msgType'   => $type,
+            'cmdId'     => $this->get_microtime() . '000000',
+            'lang'      => $config['lang'],
+            'channel'   => $config['channel'],
+            'time'      => $this->get_microtime(),
+            'appVer'    => $config['appVer'],
+            'appCode'   => $config['appCode'],
+            'deviceOS'  => $config['deviceOS'],
+            'result' => true,
+            'errorCode' => 0,
+            'errorDesc' => '',
+            'extra' => [
+                'checkSum' => $this->get_checksum($type),
+            ],
+            'momoMsg' => [
+                'ids' => [
+                    0 => $id,
+                ],
+                'bankInId'      => '',
+                '_class'        => 'mservice.backend.entity.msg.M2MUConfirmMsg',
+                'otp'           => '',
+                'otpBanknet'    => '',
+                'extras'        => '',
+            ],
+            'pass' => $config['password'],
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL             => "https://owa.momo.vn/api/$type",
             CURLOPT_RETURNTRANSFER  => true,
             CURLOPT_ENCODING        => "",
             CURLOPT_MAXREDIRS       => 10,
--- content/plugin/momo/index.php
+++ content/plugin/momo/index.php
@@ -4,7 +4,21 @@ switch ($path[2]){
         $header['title'] = 'Lịch sử giao dịch';
         require_once ABSPATH . PATH_ADMIN . "/admin-header.php";
         echo admin_breadcrumbs('MOMO', 'Lịch sử giao dịch','Lịch sử', [URL_ADMIN . "/{$path[1]}/" => 'MOMO']);
-
+        ?>
+        <div class="card">
+            <div class="header">
+                <h2>MOMO</h2>
+            </div>
+            <div class="body">
+            <?php
+                $momo = new Momo('phone', 'pass', 'otp', 'rkey', 'setupKeyEncrypted', 'imei', 'token', 'onesignalToken');
+                echo '<pre>';
+                print_r(json_decode($momo->history(2), true));
+                echo '</pre>';
+            ?>
+            </div>
+        </div>
+        <?php
         require_once ABSPATH . PATH_ADMIN . "/admin-footer.php";
         break;
 }
