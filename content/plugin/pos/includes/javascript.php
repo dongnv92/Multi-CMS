cc0aad4d07fe5150b3cda5abd43ba5c8b312355b
Dong Nguyen <nguyenvandong242@gmail.com>
2020-09-06 22:08:36 +0700

    ...

--- content/plugin/pos/includes/javascript.php
+++ content/plugin/pos/includes/javascript.php
@@ -5,12 +5,12 @@ switch ($path[2]){
         //<script>
         $(document).ready(function () {
             var countries = [
-                { value: 'Vietnam', data: 'Vietnam' },
-                { value: 'Campuchia', data: 'Campuchia' },
-                { value: 'Lao', data: 'Lao' },
-                { value: 'Thailan', data: 'Thailan' },
-                { value: 'China', data: 'China' },
-                { value: 'English', data: 'English' }
+                { value: 'Vietnam', data: 'Vietnam'},
+                { value: 'Campuchia', data: 'Campuchia'},
+                { value: 'Lao', data: 'Lao'},
+                { value: 'Thailan', data: 'Thailan'},
+                { value: 'China', data: 'China'},
+                { value: 'English', data: 'English'}
             ];
 
             $('#autocomplete').autocomplete({
