af5b788a2954cbb9aad3fadae00e85328e10462c
Dong Nguyen <nguyenvandong242@gmail.com>
2021-05-10 22:46:25 +0700

    ...

--- content/plugin/covid/index.php
+++ content/plugin/covid/index.php
@@ -103,10 +103,146 @@ switch ($path[2]){
                             'note'        => 'Thuộc diện F mấy?'
                         ])?>
                     </div>
+
+                    <!--Line 5-->
+                    <div class="col-4">
+                        <?=formInputText('covid_moiquanhe', [
+                            'label' => 'Mối quan hệ với ca bệnh tiếp xúc',
+                            'note'  => 'Mối quan hệ với ca bệnh đã tiếp xúc'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_phanloaitx', [
+                            'label' => 'Phân loại tiếp xúc',
+                            'note'  => 'Phân loại tiếp xúc'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_ngaytxcuoicung', [
+                            'placeholder' => 'Ngày tiếp xúc cuối cùng',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+
+                    <!--Line 5-->
+                    <div class="col-12">
+                        <?=formInputTextarea('covid_motahoancanhtx', [
+                            'label' => 'Mô tả hoàn cảnh tiếp xúc'
+                        ])?>
+                    </div>
+
+                    <!--Line 6-->
+                    <div class="col-4">
+                        <?=formInputText('covid_ngaykhoiphat', [
+                            'placeholder' => 'Ngày khởi phát',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_ngayvaovien', [
+                            'placeholder' => 'Ngày vào viện',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_ngayravien', [
+                            'placeholder' => 'Ngày ra viện',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+
+                    <!--Line 7-->
+                    <div class="col-4">
+                        <?=formInputText('covid_loaicanhly', [
+                            'label' => 'Loại cách ly'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_ngaycachly', [
+                            'placeholder' => 'Ngày bắt đầu cách ly',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_noicachly', [
+                            'label' => 'Địa điểm cách ly'
+                        ])?>
+                    </div>
+
+                    <!--Line 8-->
+                    <div class="col-4">
+                        <?=formInputText('covid_ngaychamdutcachly', [
+                            'placeholder' => 'Ngày chấm dứt cách ly',
+                            'layout'    => 'date'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputSelect('covid_xetnghiemchua', [
+                            ' ' => 'Đã xét nghiệm chưa',
+                            '1' => 'Chưa',
+                            '2' => 'Rồi'
+                        ], [
+                            'data-search' => 'on'
+                        ])?>
+                    </div>
+                    <div class="col-4">
+                        <?=formInputText('covid_solanxetnghiem', [
+                            'placeholder' => 'Số lần xét nghiệm'
+                        ])?>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-3">
-
+                <div class="card card-bordered">
+                    <div class="card-inner">
+                        <div class="text-center">
+                            <?=formButton('THÊM MỚI', [
+                                'id'    => 'button_covid_add',
+                                'class' => 'btn btn-secondary',
+                                'type'  => 'submit',
+                                'name'  => 'submit',
+                                'value' => 'submit'
+                            ])?>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-bordered">
+                    <div class="card-inner border-bottom">
+                        <!-- Title -->
+                        <div class="card-title-group">
+                            <div class="card-title"><h6 class="title">Thông tin người nhập</h6></div>
+                        </div>
+                        <!-- Title -->
+                    </div>
+                    <!-- Content -->
+                    <div class="card-inner">
+                        <?=formInputText('covid_user_name', [
+                            'label' => 'Họ và tên'
+                        ])?>
+                    </div>
+                    <!-- End Content -->
+                </div>
+                <div class="card card-bordered">
+                    <div class="card-inner border-bottom">
+                        <!-- Title -->
+                        <div class="card-title-group">
+                            <div class="card-title"><h6 class="title">File dữ liệu</h6></div>
+                        </div>
+                        <!-- Title -->
+                    </div>
+                    <!-- Content -->
+                    <div class="card-inner">
+                        <div class="form-group">
+                            <div class="form-control-wrap">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="meta_image" id="meta_image_add">
+                                    <label class="custom-file-label" for="meta_image_add">Chọn File</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Content -->
+                </div>
             </div>
         </div>
         <?php
